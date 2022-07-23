a3a8c8eb6ad6569485551cfd8147b583c4eb9ff4
Yasher Meeran <yasher0304@gmail.com>
2022-07-23 08:34:58 +0400

    Initial Commit

---
+++ src/components/footer/index.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex h-24 w-full items-center justify-center border-t">
+      Copyrigts @ 2022 - Yasher Meeran
+    </footer>
+  )
+}
+
+export default Footer
